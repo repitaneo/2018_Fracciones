7a661c2cf1bd86ca60cf3ffb6595d9e8619da168
Marcos Fdez <fernandez.marcos@gmail.com>
2018-11-29 12:57:00 +0100

    Getters y setters sin nada en el Start

--- src/matematicas/Fraccion.java
+++ src/matematicas/Fraccion.java
@@ -2,4 +2,23 @@ package matematicas;
 
 public class Fraccion {
 
+	private int numerador;
+	private int denominador;
+	
+	
+	public int getNumerador() {
+		return numerador;
+	}
+	public void setNumerador(int numerador) {
+		this.numerador = numerador;
+	}
+	public int getDenominador() {
+		return denominador;
+	}
+	public void setDenominador(int denominador) {
+		this.denominador = denominador;
+	}
+	
+	
+	
 }
