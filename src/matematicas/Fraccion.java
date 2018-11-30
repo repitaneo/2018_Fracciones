728de7567d45c5c2342fc5ea4d33252876b2fb2c
Marcos Fdez <marcosd@educastur.org>
2018-11-30 11:00:16 +0100

    Operaciones suma resta multiplicar y dividir

--- src/matematicas/Fraccion.java
+++ src/matematicas/Fraccion.java
@@ -2,10 +2,21 @@ package matematicas;
 
 public class Fraccion {
 
+	
+	/*
+	 * ATRIBUTOS
+	 * 
+	 */
 	private int numerador;
 	private int denominador;
 	
 	
+	
+	/*
+	 * SETTERS Y GETTERS
+	 * 
+	 * 
+	 */
 	public int getNumerador() {
 		return numerador;
 	}
@@ -16,8 +27,114 @@ public class Fraccion {
 		return denominador;
 	}
 	public void setDenominador(int denominador) {
-		this.denominador = denominador;
+		
+		if(denominador!=0) {
+			this.denominador = denominador;
+		}
 	}
+	
+	
+	/*
+	 * 
+	 * OPERACION DE MULTIPLICAR
+	 * 
+	 */
+	public Fraccion multiplicar(Fraccion otra) {
+		
+		int nuevoNumerador = this.numerador * otra.numerador;
+		int nuevoDenominador = this.denominador * otra.denominador;
+
+		Fraccion devolver = new Fraccion();
+		devolver.numerador = nuevoNumerador;
+		devolver.denominador = nuevoDenominador;
+		
+		return devolver;
+	}
+	
+	
+	/*
+	 * 
+	 * OPERACION DE MULTIPLICAR
+	 * 
+	 */
+	public Fraccion dividir(Fraccion otra) {
+		
+		int nuevoNumerador = this.numerador * otra.denominador;
+		int nuevoDenominador = this.denominador * otra.numerador;
+
+		Fraccion devolver = new Fraccion();
+		devolver.numerador = nuevoNumerador;
+		devolver.denominador = nuevoDenominador;
+		
+		return devolver;
+	}	
+	
+	
+	
+	
+	
+	/*
+	 * 
+	 * OPERACION DE SUMAR
+	 * 
+	 */
+	public Fraccion suma(Fraccion otra) {
+		
+		int nuevoNumerador = this.numerador*otra.denominador + otra.numerador*this.denominador;
+		int nuevoDenominador = this.denominador * otra.denominador;
+
+		Fraccion devolver = new Fraccion();
+		devolver.numerador = nuevoNumerador;
+		devolver.denominador = nuevoDenominador;
+		
+		return devolver;
+	}	
+	
+	
+	
+	
+	/*
+	 * 
+	 * OPERACION DE RESTA
+	 * 
+	 */
+	public Fraccion resta(Fraccion otra) {
+		
+		int nuevoNumerador = this.numerador*otra.denominador - otra.numerador*this.denominador;
+		int nuevoDenominador = this.denominador * otra.denominador;
+
+		Fraccion devolver = new Fraccion();
+		devolver.numerador = nuevoNumerador;
+		devolver.denominador = nuevoDenominador;
+		
+		return devolver;
+	}	
+	
+	
+	
+	private void reducir() {
+		
+
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public String toString() {
+		
+		return numerador+"/"+denominador;
+	}
+	
+	
+	
 	
 	
 	
--- src/matematicas/Start.java
+++ src/matematicas/Start.java
@@ -1,10 +1,29 @@
 package matematicas;
 
+
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		Fraccion unaFraccion = new Fraccion();
+		unaFraccion.setNumerador(1);
+		unaFraccion.setDenominador(8);
+		System.out.println(unaFraccion);
+		
+		Fraccion unaSegundaFraccion = new Fraccion();
+		unaSegundaFraccion.setNumerador(1);
+		unaSegundaFraccion.setDenominador(2);
+		System.out.println(unaSegundaFraccion);
+		
+		
+		
+		Fraccion resultado;
+		resultado = unaSegundaFraccion.suma(unaFraccion);
+		System.out.println(resultado);
+		
+		
 	}
 
 }
